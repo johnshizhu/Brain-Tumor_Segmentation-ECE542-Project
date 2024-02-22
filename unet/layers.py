a7d6ed94c1e6088148959be72caf2495b282bc82
johnshizhu <johnshizhu@gmail.com>
2024-02-22 12:13:27 -0500

    finished all block components to UNET including decoder and encoding blocks

--- unet/layers.py
+++ unet/layers.py
@@ -4,24 +4,21 @@ import torch.nn as nn
 
 def addBlock(sequence, in_channels, out_channels, kernel_size, dropout=0.1, stride=1, padding=0, conv3d=True):
     '''
-    Adds a Convolutional Block, with optional batch normalization, ReLU, and dropout.
+    Adds a convolutional block to a neural network sequence based on a given configuration.
+    
     Args:
-     - sequence (string): sequence of 'c', 'b', 'r', 'd'
-        - 'c': convolution 
-        - 'b': batch norm
-        - 'r': ReLU  
-        - 'd': dropout
-     - in_channels (int): Number of input channels
-     - out_channels (int): Number of output channels
-     - kernel_size (int or tuple): Size of the convolutional kernel
-     - stride (int or tuple, optional): Stride of the convolution (default: 1)
-     - padding (int or tuple, optional): Padding of the convolution (default: 0)
-     - dropout (float, optional): Dropout rate (default: 0.1)
-     - conv3d (boolean, optional): 
-        - True: 3D Convolution (default)
-        - False: 2D Convolution 
+        sequence (str): A string sequence consisting of 'c' (convolution), 'b' (batch normalization), 
+                        'r' (ReLU activation), and 'd' (dropout) to specify the layers and their order.
+        in_channels (int): Number of input channels for the convolutional layer.
+        out_channels (int): Number of output channels for the convolutional layer.
+        kernel_size (int or tuple): Size of the kernel for the convolutional layers.
+        dropout (float, optional): Dropout rate for dropout layers. Default is 0.1.
+        stride (int or tuple, optional): Stride for the convolutional layers. Default is 1.
+        padding (int or tuple, optional): Padding for the convolutional layers. Default is 0.
+        conv3d (bool, optional): Flag to determine whether to use 3D convolutions (True) or 2D convolutions (False). Default is True.
+
     Returns:
-     - modules (list): List of PyTorch modules representing the convolutional block
+        modules (list of nn.Module): List of PyTorch modules representing the constructed convolutional block.
     '''
     modules = []
     for i in sequence:
@@ -53,18 +50,17 @@ def addBlock(sequence, in_channels, out_channels, kernel_size, dropout=0.1, stri
 
 class ConvSingle(nn.Sequential):
     '''
-    Single Convolutional Layer
-    conv --> batch norm --> ReLU --> dropout(optional)
+    Single Convolutional Layer module, encapsulating a standard set of layers: Convolution, Batch Normalization,
+    ReLU activation, and optionally Dropout, based on the given architecture sequence.
+    
     Args:
-     - in_channels: 
-     - out_channels: 
-     - kernel_size: 
-     - stride: 
-     - padding: 
-     - dropout: 
-     - conv3d (boolean): 
-        - True: 3D Convolution 
-        - False: 2D Convolution
+        in_channels (int): Number of input channels to the convolutional layer.
+        out_channels (int): Number of output channels from the convolutional layer.
+        kernel_size (int or tuple): Size of the convolutional kernel.
+        dropout (float): Dropout rate. If dropout is 0, no dropout layer is added.
+        stride (int or tuple): Stride for the convolutional layer.
+        padding (int or tuple): Padding for the convolutional layer.
+        conv3d (bool): Indicates whether to use 3D convolution (True) or 2D convolution (False).
     '''
     def __init__(self, in_channels, out_channels, kernel_size, dropout, stride, padding, conv3d):
         super(ConvSingle, self).__init__()
@@ -77,20 +73,19 @@ class ConvSingle(nn.Sequential):
 
 class ConvDouble(nn.Sequential):
     '''
-    Double Convolutional Layer
+    Double Convolutional Layer module, comprising two sets of convolutional layers each followed by batch normalization
+    and ReLU activation. Optionally, a dropout layer can be included after each ReLU activation.
+    
     Args:
-     - in_channels: 
-     - out_channels: 
-     - kernel_size: 
-     - endec (boolean):
-        - True: encoder part of network
-        - False: decoder part of network
-     - stride: 
-     - padding: 
-     - dropout: 
-     - conv3d (boolean): 
-        - True: 3D Convolution 
-        - False: 2D Convolution
+        in_channels (int): Number of input channels to the first convolutional layer.
+        out_channels (int): Number of output channels from the second convolutional layer.
+        kernel_size (int or tuple): Size of the convolutional kernels.
+        endec (bool): Indicates whether the block is part of the encoder (True) or decoder (False). 
+                      This influences intermediate channel sizes.
+        dropout (float): Dropout rate for dropout layers.
+        stride (int or tuple): Stride for the convolutional layers.
+        padding (int or tuple): Padding for the convolutional layers.
+        conv3d (bool): Indicates whether to use 3D convolutions (True) or 2D convolutions (False).
     '''
     def __init__(self, in_channels, out_channels, kernel_size, endec, dropout, stride, padding, conv3d):
         super(ConvDouble, self).__init__()
@@ -105,17 +100,18 @@ class ConvDouble(nn.Sequential):
 
 class EncoderBlock(nn.module):
     '''
-    Encoder Block consists of 
-    Double Conv Layer --> ReLU Activation function --> Max Pool
+    Encoder Block consisting of a double convolutional layer followed by a Max Pool operation. 
+    This block also saves its output before pooling for use as a skip connection in a corresponding Decoder Block.
+    
     Args:
-        in_channels (int): Number of input channels.
-        out_channels (int): Number of output channels.
-        conv_kernel_size (int or tuple): Size of the convolutional kernel.
-        pool_kernel_size (int or tuple): Size of the pooling kernel.
-        dropout (float): Dropout probability.
-        conv_stride (int or tuple): Stride of the convolution.
-        conv_padding (int or tuple): Padding applied to the convolution.
-        conv3d (bool): If True, 3D convolution and pooling are applied. If False, 2D convolution and pooling are applied.    
+        in_channels (int): Number of input channels to the block.
+        out_channels (int): Number of output channels from the block.
+        conv_kernel_size (int or tuple): Kernel size for the convolutional layers.
+        pool_kernel_size (int or tuple): Kernel size for the max pooling operation.
+        dropout (float): Dropout rate for dropout layers within the block.
+        conv_stride (int or tuple): Stride for the convolutional layers.
+        conv_padding (int or tuple): Padding for the convolutional layers.
+        conv3d (bool): Indicates whether to use 3D convolutions and pooling (True) or 2D (False).
     '''
     def __init__(self, in_channels, out_channels, conv_kernel_size, pool_kernel_size, dropout, conv_stride, conv_padding, conv3d):
         super(EncoderBlock, self).__init__()
@@ -136,18 +132,18 @@ class EncoderBlock(nn.module):
 
 class DecoderBlock(nn.module):
     '''
-    Decoder Block consists of
-    Upscaling --> Double Conv Layer
-
+    Decoder Block consisting of an upscaling operation followed by a double convolutional layer. 
+    This block combines upsampled features with features from a corresponding Encoder Block via a skip connection.
+    
     Args:
-        in_channels (int): Number of input channels.
-        out_channels (int): Number of output channels.
-        conv_kernel_size (int or tuple): Size of the convolutional kernel for the double convolution.
-        up_kernel_size (int or tuple): Size of the transposed convolutional kernel for upscaling.
-        dropout (float): Dropout probability.
-        conv_stride (int or tuple): Stride of the convolution for the double convolution.
-        conv_padding (int or tuple): Padding applied to the convolution for the double convolution.
-        conv3d (bool): If True, 3D convolution and transpose convolution are applied. If False, 2D convolution and transpose convolution are applied.
+        in_channels (int): Number of input channels to the block.
+        out_channels (int): Number of output channels from the block.
+        conv_kernel_size (int or tuple): Kernel size for the convolutional layers.
+        up_kernel_size (int or tuple): Kernel size for the transposed convolutional layer used for upscaling.
+        dropout (float): Dropout rate for dropout layers within the block.
+        conv_stride (int or tuple): Stride for the convolutional layers.
+        conv_padding (int or tuple): Padding for the convolutional layers.
+        conv3d (bool): Indicates whether to use 3D transposed convolutions and convolutions (True) or 2D (False).
     '''
     def __init__(self, in_channels, out_channels, conv_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d):
         super(DecoderBlock, self).__iniit__()
@@ -157,29 +153,64 @@ class DecoderBlock(nn.module):
         else:
             self.upScale = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=up_kernel_size)
 
-    def forward(self, x):
+    def forward(self, x, skip_connection):
         post_ups_features = self.upScale(x)
-        post_conv_features = self.doubleConv(post_ups_features)
+        cat_features = torch.cat((post_ups_features, skip_connection), dim=1)
+        post_conv_features = self.doubleConv(cat_features)
         return post_conv_features
 
 class EncoderUNet(nn.module):
     '''
-    UNet Encoder made of Encoder blocks
+    UNet Encoder composed of a series of Encoder Blocks. Each block applies a double convolution followed by max pooling,
+    and stores the pre-pooled feature maps for use as skip connections in the decoder.
+    
+    Args:
+        in_channels (int): Number of input channels to the first encoder block.
+        out_channels (int): Number of output channels for each encoder block (assumes incrementing by block).
+        conv_kernel_size (int or tuple): Convolutional kernel size for all encoder blocks.
+        pool_kernel_size (int or tuple): Pooling kernel size for all encoder blocks.
+        dropout (float): Dropout rate applied within each encoder block.
+        conv_stride (int or tuple): Stride for the convolutional operations within each block.
+        conv_padding (int or tuple): Padding for the convolutional operations within each block.
+        conv3d (bool): Flag indicating whether the encoder uses 3D convolution (True) or 2D (False).
+        size (int): Number of encoder blocks in the UNet.
     '''
-    def __init__(self, args):
+    def __init__(self, in_channels, out_channels, conv_kernel_size, pool_kernel_size, dropout, conv_stride, conv_padding, conv3d, size):
         super(EncoderUNet, self).__init__()
+        self.blocks = nn.ModuleList()
+        for i in range(size):
+            self.blocks.append(EncoderBlock(in_channels, out_channels, conv_kernel_size, pool_kernel_size, dropout, conv_stride, conv_padding, conv3d))
 
     def forward(self, x):
-        # FIX
-        return x
+        skip_connections = []
+        for block in self.blocks:
+            x = block(x)
+            skip_connections.append(block.skip_features)
+        return x, skip_connections
 
 class DecoderUNet(nn.module):
     '''
-    UNet Decoder made of Decoder blocks
+    UNet Decoder composed of a series of Decoder Blocks. Each block upscales the feature map and merges it with a corresponding
+    feature map from the Encoder through skip connections, followed by a double convolution.
+    
+    Args:
+        in_channels (int): Number of input channels to the first decoder block.
+        out_channels (int): Number of output channels for each decoder block (assumes decrementing by block).
+        conv_kernel_size (int or tuple): Convolutional kernel size for all decoder blocks.
+        up_kernel_size (int or tuple): Upscaling kernel size for all decoder blocks.
+        dropout (float): Dropout rate applied within each decoder block.
+        conv_stride (int or tuple): Stride for the convolutional operations within each block.
+        conv_padding (int or tuple): Padding for the convolutional operations within each block.
+        conv3d (bool): Flag indicating whether the decoder uses 3D convolution (True) or 2D (False).
+        size (int): Number of decoder blocks in the UNet.
     '''
-    def __init__(self, args):
+    def __init__(self, in_channels, out_channels, conv_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d, size):
         super(DecoderUNet, self).__init__()
+        self.blocks = nn.ModuleList()
+        for i in range(size):
+            self.blocks.append(DecoderBlock(in_channels, out_channels, conv_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d))
 
-    def forward(self, x):
-        # FIX
+    def forward(self, x, skip_connections):
+        for block, skip_connection in zip(self.blocks, reversed(skip_connections)):
+            x = block(x, skip_connection)
         return x
--- unet/model.py
+++ unet/model.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn as nn
-from layers import EncoderBlock, DecoderBlcok, EncoderUNet, DecoderUNet
+from layers import EncoderBlock, DecoderBlock, EncoderUNet, DecoderUNet
 
 class GeneralUNet(nn.Module):
     '''
     General class for UNet Structure
     '''
-    def __init__(self, in_channels, out_channels, size, args):
+    def __init__(self, in_channels, out_channels, conv_kernel_size, pool_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d, size):
         super(GeneralUNet, self).__init__()
-
+        self.encoder_series = EncoderUNet(in_channels, out_channels, conv_kernel_size, pool_kernel_size, dropout, conv_stride, conv_padding, conv3d, size)
+        self.decoder_series = DecoderUNet(in_channels, out_channels, conv_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d)
 
     def forward(self, x):
         encoder_features = self.encoder(x)
