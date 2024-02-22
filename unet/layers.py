60a2c8ddec74a1d57efde87d03e51c5a17600950
johnshizhu <johnshizhu@gmail.com>
2024-02-22 12:42:11 -0500

    finished basic general architecture code, still need specfiic 3d and 2d classes (based on general class)

--- unet/layers.py
+++ unet/layers.py
@@ -214,3 +214,32 @@ class DecoderUNet(nn.module):
         for block, skip_connection in zip(self.blocks, reversed(skip_connections)):
             x = block(x, skip_connection)
         return x
+    
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dropout, conv3d):
+        super(Bottleneck, self).__init__()
+        if conv3d:
+            self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size, stride=stride, padding=padding)
+            self.relu1 = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size, stride=stride, padding=padding)
+            self.relu2 = nn.ReLU(inplace=True)
+            self.bn    = nn.BatchNorm3d(out_channels)
+            if dropout:
+                self.dropout = nn.Dropout3d(p=dropout)
+        else:
+            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding)
+            self.relu1 = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride=stride, padding=padding)
+            self.relu2 = nn.ReLU(inplace=True)
+            self.bn    = nn.BatchNorm3d(out_channels)
+            if dropout:
+                self.dropout = nn.Dropout2d(p=dropout)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.bn(x)
+        x = self.dropout(x)
+        return x
--- unet/model.py
+++ unet/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from layers import EncoderBlock, DecoderBlock, EncoderUNet, DecoderUNet
+from layers import EncoderBlock, DecoderBlock, EncoderUNet, DecoderUNet, Bottleneck
 
 class GeneralUNet(nn.Module):
     '''
@@ -9,11 +9,13 @@ class GeneralUNet(nn.Module):
     def __init__(self, in_channels, out_channels, conv_kernel_size, pool_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d, size):
         super(GeneralUNet, self).__init__()
         self.encoder_series = EncoderUNet(in_channels, out_channels, conv_kernel_size, pool_kernel_size, dropout, conv_stride, conv_padding, conv3d, size)
+        self.bottleneck     = Bottleneck(in_channels, out_channels, conv_kernel_size, conv_stride, conv_padding, dropout, conv3d)
         self.decoder_series = DecoderUNet(in_channels, out_channels, conv_kernel_size, up_kernel_size, dropout, conv_stride, conv_padding, conv3d)
 
     def forward(self, x):
-        encoder_features = self.encoder(x)
-        output_features = self.decoder(encoder_features)
+        encoder_features, skip_connections = self.encoder_series(x)
+        bottle_features = self.bottleneck(encoder_features)
+        output_features = self.decoder_series(bottle_features, skip_connections)
         return output_features
 
 class UNet3D(GeneralUNet):
