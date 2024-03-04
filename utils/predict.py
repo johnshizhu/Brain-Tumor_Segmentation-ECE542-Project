7abd9e034e3800e4db02c8db4c7ab33d2dfe3253
Colin O'Dowd <cdodowdee@gmail.com>
2024-03-03 19:12:26 -0500

    setup for overlays

--- utils/predict.py
+++ utils/predict.py
@@ -2,14 +2,21 @@ import os
 import torch
 import numpy as np
 import nibabel as nib
-import matplotlib.pyplot as plt
 
 class ModelPredict:
-    def __init__(self, model, device):
-        self.model = model
-        self.device = device
-        self.model.eval()  
+    def __init__(self, model_path):
+        self.model = torch.load(model_path)
+        self.model.eval()
     
-    def predict(self, scan):
+    def get_scan(self, scan_folder, scan_type, index):
+        scan_path = os.path.join(scan_folder, f'BraTS20_Training_{index+1:03d}_{scan_type}.nii')
+        scan = nib.load(scan_path)
+        scan_data = scan.get_fdata()
         
-
+        return scan_data
+    
+    def predict(self, input_tensor):
+        with torch.no_grad():  
+            prediction = self.model(input_tensor)
+        
+        return prediction
